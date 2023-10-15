2415441e60412f55f5ec73a8a99c853ae725bd5c
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-10-15 20:24:18 +0200

    frontend

--- edunova-pwa/src/Components/Kazeta/Kazete.component.js
+++ edunova-pwa/src/Components/Kazeta/Kazete.component.js
@@ -1,15 +1,53 @@
-import React, { Component } from "react";
-import { Container, Table } from "react-bootstrap";
-
+import React, {Component} from "react";
+import {Button, Container, Table} from "react-bootstrap";
+import KazetaDataService from "../../services/kazeta.service";
+import {NumericFormat} from "react-number-format";
+import {Link} from "react-router-dom";
+import {FaEdit, FaTrash} from "react-icons/fa"
 
 export default class kazeta extends Component{
 
+    constructor(props){
+        super(props);
 
+        this.state={
+            kazeta: []
+        }
+    }
+
+componentDidMount(){
+    this.dohvatiKazetu();
+}
+
+async dohvatiKazetu(){
+    await KazetaDataService.get()
+    .then(response=> {
+        this.setState({
+            kazeta:response.data
+        });
+        console.log(response.data);   
+    })
+    .catch(e=>{
+        console.log(e);
+    });
+}
+
+async obrisikazetu(sifra){
+    const odgovor=await KazetaDataService.delete(sifra);
+    if(odgovor.ok){
+        this.dohvatiKazetu();
+    }else{
+        alert(odgovor.poruka);
+    }
+}
     render(){
+        
+        const {kazeta} = this.state;
+
         return (
             <Container>
-               <a href="/smjerovi/dodaj" className="btn btn-success gumb">
-                Dodaj novi smjer
+               <a href="/Kazeta/dodaj" className="btn btn-success gumb">
+                Dodaj novu kazetu 
                </a>
                 
                <Table striped bordered hover responsive>
